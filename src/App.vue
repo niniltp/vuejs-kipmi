0d15789d4a1caa10bf8bfb227e864b35e2424a37
niniltp <nhienphan923@gmail.com>
2019-07-22 18:04:34 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <notifications group="general"
+                   position="top right"
+                   :speed="700" :duration="6000" :width="400" class="notification-title notification-content"/>
+    <h1>Kipmi</h1>
+    <Quotes v-on:show="show"></Quotes>
+  </div>
+</template>
+
+<script>
+import Quotes from './components/Quotes.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Quotes
+  },
+  methods: {
+    show (notif) {
+      this.$notify(notif)
+    },
+    clean (group) {
+      this.$notify({ group, clean: true })
+    }
+  }
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+  @import "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css";
+  @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";                                                                                                                                                                                                                                                                                                                                              <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+  body {
+    background-color: #EEEEEE;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.3em;
+    display: grid;
+    grid-template-rows: auto;
+    justify-items: center;
+    align-items: center;
+  }
+  body, html {
+    margin: 0;
+    height: 100%;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 4em;
+    margin: 30px;
+  }
+
+  .notification-title {
+    font-size: 2em;
+  }
+
+  .notification-content {
+    font-size: 1.5em;
+  }
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="quote">
+        <blockquote class="blockquote">
+            <div class="text">
+                <h3 v-if="quote.title !== ''">{{quote.title}}</h3>
+                <p><q>{{quote.content}}</q></p>
+                <footer class="blockquote-footer">
+                    <span v-if="quote.author === ''">Unknown</span><span v-else>{{quote.author}}</span>
+                    <span v-if="quote.from !== ''">, from <cite>{{quote.from}}</cite></span>
+                </footer>
+            </div>
+            <i v-if="quote.hover == true" class="fa fa-minus-circle fa-2x destroy" @click="$emit('removeQuote', quote)"></i>
+        </blockquote>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'quotes',
+        props: ['quote']
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+
+    h3 {
+        text-align: center;
+        font-weight: bold;
+        padding: 30px 0 50px;
+    }
+
+    h3::first-letter {
+        text-transform: uppercase;
+    }
+
+    .quote {
+        padding: 10px 0px 20px 0px;
+    }
+
+    .text > p > q {
+        quotes: '\00AB ' ' \00BB';
+        color: gray;
+        font-size: 1em;
+        font-weight: 400;
+    }
+
+    .text > p {
+        text-align: justify;
+        text-justify: auto;
+        white-space: pre-wrap;
+    }
+
+
+    .blockquote {
+        border: none;
+        padding-bottom: 50px;
+    }
+
+    .text {
+        width: 90%;
+        float: left;
+    }
+
+    .blockquote-footer {
+        text-align: right;
+        font-size: 1em;
+    }
+
+    .destroy {
+        float: right;
+        cursor: pointer;
+    }
+
+
+</style>
